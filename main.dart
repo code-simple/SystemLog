4103d0b56a5fc93b552f62070019523bfc198166
code-simple <61619216+code-simple@users.noreply.github.com>
2020-10-08 14:11:27 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -8,7 +8,7 @@ main() async {
   var f = File("Dart_time_log.doc");
   f.writeAsString(
       await 'System Started: ' +
-          DateFormat(' hh:m a  dd/MMM/yyyy').format(DateTime.now()) +
+          DateFormat(' hh:mm a  dd/MMM/yyyy').format(DateTime.now()) +
           '\n',
       mode: FileMode.append);       // Mode append used to append next line to log file.
 }
